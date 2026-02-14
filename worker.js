4e145eaa0195aa8d9e7a05342ae505dc3df6c826
Sharvi <heysharvi@gmail.com>
2026-02-14 22:28:15 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,33 +1,33 @@
 importScripts("https://cdn.jsdelivr.net/npm/javascript-obfuscator/dist/index.browser.js");
 
 self.onmessage = function(e) {
-    const code = e.data;
+    let code = e.data;
 
     try {
+        // Remove BOM
+        code = code.replace(/^\uFEFF/, "");
 
-       const isLarge = code.length > 500000;
+        const isLarge = code.length > 500000;
 
-const options = isLarge
-    ? {
-        compact: true,
-        stringArray: true,
-        rotateStringArray: true,
-        target: 'browser',
-        ignoreImports: true
-    }
-    : {
-        compact: true,
-        controlFlowFlattening: true,
-        deadCodeInjection: true,
-        stringArray: true,
-        stringArrayEncoding: ['base64'],
-        rotateStringArray: true,
-        selfDefending: true,
-        target: 'browser',
-        ignoreImports: true
-    };
-
-let cleanedCode = code.replace(/^\uFEFF/, "");
+        const options = isLarge
+            ? {
+                compact: true,
+                stringArray: true,
+                rotateStringArray: true,
+                target: 'browser',
+                ignoreImports: true
+            }
+            : {
+                compact: true,
+                controlFlowFlattening: true,
+                deadCodeInjection: false,
+                stringArray: true,
+                stringArrayEncoding: ['base64'],
+                rotateStringArray: true,
+                selfDefending: false,
+                target: 'browser',
+                ignoreImports: true
+            };
 
         const result = JavaScriptObfuscator.obfuscate(code, options);
 
